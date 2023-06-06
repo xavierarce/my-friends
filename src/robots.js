b0bdb515a58e956b9e44a6696608cb2b1631e45a
xavierarce <119446640+xavierarce@users.noreply.github.com>
2023-06-06 15:31:25 -0500

    main

--- src/robots.js
+++ src/robots.js
@@ -1,63 +1,9 @@
 export const robots = [
   {
-    id: 1,
-    name: 'Leanne Graham',
-    username: 'Bret',
-    email: 'Sincere@april.biz'
-  },
-  {
     id: 2,
     name: 'Xavier Arce',
     username: 'Xavier',
     email: 'xaviarcea@7meses.com'
-  },
-  {
-    id: 3,
-    name: 'Clementine Bauch',
-    username: 'Samantha',
-    email: 'Nathan@yesenia.net'
-  },
-  {
-    id: 4,
-    name: 'Patricia Lebsack',
-    username: 'Karianne',
-    email: 'Julianne.OConner@kory.org'
-  },
-  {
-    id: 5,
-    name: 'Chelsey Dietrich',
-    username: 'Kamren',
-    email: 'Lucio_Hettinger@annie.ca'
-  },
-  {
-    id: 6,
-    name: 'Mrs. Dennis Schulist',
-    username: 'Leopoldo_Corkery',
-    email: 'Karley_Dach@jasper.info'
-  },
-  {
-    id: 7,
-    name: 'Kurtis Weissnat',
-    username: 'Elwyn.Skiles',
-    email: 'Telly.Hoeger@billy.biz'
-  },
-  {
-    id: 8,
-    name: 'Nicholas Runolfsdottir V',
-    username: 'Maxime_Nienow',
-    email: 'Sherwood@rosamond.me'
-  },
-  {
-    id: 9,
-    name: 'Glenna Reichert',
-    username: 'Delphine',
-    email: 'Chaim_McDermott@dana.io'
-  },
-  {
-    id: 10,
-    name: 'Clementina DuBuque',
-    username: 'Moriah.Stanton',
-    email: 'Rey.Padberg@karina.biz'
   },
   {
     id: 11,
@@ -65,4 +11,58 @@ export const robots = [
     username: 'Reina de mi Corazon',
     email: 'sthefanymireinaApolo@7meses.com'
   },
+  {
+    id: 12,
+    name: faker.name.findName(),
+    username: faker.internet.userName(),
+    email: faker.internet.email()
+  },
+  {
+    id: 13,
+    name: faker.name.findName(),
+    username: faker.internet.userName(),
+    email: faker.internet.email()
+  },
+  {
+    id: 14,
+    name: faker.name.findName(),
+    username: faker.internet.userName(),
+    email: faker.internet.email()
+  },
+  {
+    id: 15,
+    name: faker.name.findName(),
+    username: faker.internet.userName(),
+    email: faker.internet.email()
+  },
+  {
+    id: 16,
+    name: faker.name.findName(),
+    username: faker.internet.userName(),
+    email: faker.internet.email()
+  },
+  {
+    id: 17,
+    name: faker.name.findName(),
+    username: faker.internet.userName(),
+    email: faker.internet.email()
+  },
+  {
+    id: 18,
+    name: faker.name.findName(),
+    username: faker.internet.userName(),
+    email: faker.internet.email()
+  },
+  {
+    id: 19,
+    name: faker.name.findName(),
+    username: faker.internet.userName(),
+    email: faker.internet.email()
+  },
+  {
+    id: 20,
+    name: faker.name.findName(),
+    username: faker.internet.userName(),
+    email: faker.internet.email()
+  }
 ];
