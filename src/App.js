b649b79621653b153d516fbc0b6a7386e4a0f476
xavierarce <119446640+xavierarce@users.noreply.github.com>
2023-06-06 14:45:15 -0500

    main

--- src/App.js
+++ src/App.js
@@ -10,27 +10,36 @@ class App extends Component {
     constructor(){
         super()
         this.state = {
-            robots : robots,
+            robots : [],
             searchfield : ''
         }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response=> response.json())
+            .then(users=> this.setState({robots:users }));
     }
 
     onSearchChange = (event) => {
         this.setState({searchfield: event.target.value})
     }    
 
-
     render(){
         const filteredRobots = this.state.robots.filter(robot =>{
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return(
-            <div className="tc">
-                <h1 className="f1">Robofriends</h1>
-                <Searchbox searchChange={this.onSearchChange}/>
-                <Cardlist robots={filteredRobots}/>
-            </div>
-        )
+        if(this.state.robots.length===0){
+            return <h1 className="tc"> Loaiding </h1>
+        } else{
+            return(
+                <div className="tc">
+                    <h1 className="f1">Robofriends</h1>
+                    <Searchbox searchChange={this.onSearchChange}/>
+                    <Cardlist robots={filteredRobots}/>
+                </div>
+            );
+        };
     }; 
 }
 
